f48eef3b721249f5412848c4db1e00d9accfd4c6
Sagar-DU <nahidhasansagar@gmail.com>
2024-11-16 18:30:32 +0600

    leet accapted this solution

--- Leet Code/palindrome_number.py
+++ Leet Code/palindrome_number.py
@@ -8,3 +8,4 @@ elif (int(x) == int(reverse)):
     print ("Palindrome!")
 else:
     print ("The input number is not a Palindrome.")
+# THIS IS MY CODE THIS CODE HAS A PROLBEM THAT IF I INPUT A STRING THEN THE CODE GIVES ERROR AND I CAN'T ADDRESS THAT 
